63fbdb67b4bb2bfc9157a93a3cf45e9fcad9d6ca
Marina <umarishka@bk.ru>
2022-09-06 23:15:29 +0300

    add N41 N43

---
+++ N41/Program.cs
@@ -0,0 +1,20 @@
+﻿ /* Пользователь вводит с клавиатуры M чисел. 
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+/* Console.WriteLine("введите несколько чисел");
+int size = int.Parse(Console.ReadLine());
+int [] array = new int [size];
+for (int i =0; i<array.Length; ++i ); */
+
+Console.Write("Введите количество чисел : ");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < number; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
