43ea8f13a7ed46b704fc2aa7c383386fa483265f
Marina <umarishka@bk.ru>
2022-09-06 23:20:05 +0300

    replace N41

--- N41/Program.cs
+++ N41/Program.cs
@@ -2,11 +2,6 @@
  Посчитайте, сколько чисел больше 0 ввёл пользователь.
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3 */
-/* Console.WriteLine("введите несколько чисел");
-int size = int.Parse(Console.ReadLine());
-int [] array = new int [size];
-for (int i =0; i<array.Length; ++i ); */
-
 Console.Write("Введите количество чисел : ");
 int number = int.Parse(Console.ReadLine());
 int count = 0;
